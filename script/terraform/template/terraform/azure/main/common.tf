37fb675ba545884393e47ad05d439584b3cd9348
Wang, Sunny <sunny.wang@intel.com>
2023-05-16 14:36:40 +0800

    Delete unnecessary files.

--- script/terraform/template/terraform/azure/main/common.tf
+++
@@ -1,5 +0,0 @@
-resource "azurerm_resource_group" "default" {
-  name     = "wsf-${var.job_id}-rg"
-  location = var.region!=null?var.region:replace(var.zone,"/^(.*)..$/","$1")
-  tags     = var.common_tags
-}
